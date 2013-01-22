e6901207494b752f0227c2bb6bfa89bbbaee8c44
Damien Mayance <valryon@gmail.com>
2013-01-22 13:51:42 +0100

    Creating empty projects

---
+++ ws/SuperKoikoukesse.Webservice/Global.asax.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace SuperKoikoukesse.Webservice
+{
+    // Remarque : pour obtenir des instructions sur l'activation du mode classique IIS6 ou IIS7, 
+    // visitez http://go.microsoft.com/?LinkId=9394801
+
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+                "Default", // Nom d'itinéraire
+                "{controller}/{action}/{id}", // URL avec des paramètres
+                new { controller = "Home", action = "Index", id = UrlParameter.Optional } // Paramètres par défaut
+            );
+
+        }
+
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+
+            // Utiliser LocalDB pour Entity Framework par défaut
+            Database.DefaultConnectionFactory = new SqlConnectionFactory(@"Data Source=(localdb)\v11.0; Integrated Security=True; MultipleActiveResultSets=True");
+
+            RegisterGlobalFilters(GlobalFilters.Filters);
+            RegisterRoutes(RouteTable.Routes);
+        }
+    }
+}
