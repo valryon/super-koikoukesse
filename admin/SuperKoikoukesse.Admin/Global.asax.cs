e3bf0a8d05e013f12c79deaba200f9a048e1eb5a
Damien Mayance <valryon@gmail.com>
2013-01-25 17:29:56 +0100

    Clean ASP.NET MVC site template with bootstrap & jquery

--- admin/SuperKoikoukesse.Admin/Global.asax.cs
+++ admin/SuperKoikoukesse.Admin/Global.asax.cs
@@ -1,9 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Data.Entity;
-using System.Data.Entity.Infrastructure;
-using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
 
@@ -34,9 +29,6 @@ namespace SuperKoikoukesse.Admin
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
-
-            // Utiliser LocalDB pour Entity Framework par défaut
-            Database.DefaultConnectionFactory = new SqlConnectionFactory(@"Data Source=(localdb)\v11.0; Integrated Security=True; MultipleActiveResultSets=True");
 
             RegisterGlobalFilters(GlobalFilters.Filters);
             RegisterRoutes(RouteTable.Routes);
