d74812f6111ec57806773d4a9978fd08121195a9
ashen <yiashen@gmail.com>
2013-03-28 16:11:07 +0100

    Color constants for design
    * Basics

---
+++ game/SuperKoikoukesse.iOS/Views/ColorConstants.cs
@@ -0,0 +1,21 @@
+using System;
+
+namespace SuperKoikoukesse.iOS
+{
+	public class ColorConstants
+	{
+		public static string BRAND_WHITE	= "#D8D8D8";
+		public static string BRAND_BLUE 	= "#1F79CA";
+		public static string BRAND_GREY 	= "#E6E6E6";
+		public static string BRAND_BLACK 	= "#3E3E3E";
+
+		public static string TEXT_WHITE		= "#D8D8D8";
+		public static string TEXT_BLACK     = "#4A4A4A";
+
+		public static string COMBO_LEVEL_1  = "#64A4DC";
+		public static string COMBO_LEVEL_2  = "#3F99EA";
+		public static string COMBO_LEVEL_3  = "#1F79CA";
+		public static string COMBO_LEVEL_4  = "#1560A3";
+	}
+}
+
