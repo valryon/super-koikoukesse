7f4be05c95914e6bad7c4fab088536723d1c0948
Damien Mayance <valryon@gmail.com>
2013-06-21 16:46:29 +0200

    .NET -> Ruby (WIP)

---
+++ ws/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ws::Application.config.secret_token = 'a4e26a01c3d1185261eb8f76259431eeb57a3f355e5b5eca5fc1ae4a195936646fe3e7c1b6adee6712e11fabe113e296abab7eae14446f54e2f7d919e505b8e2'
