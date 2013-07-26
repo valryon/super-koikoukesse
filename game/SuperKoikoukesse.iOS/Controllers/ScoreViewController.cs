13298ba828eb4da0e93e8ff95eee37f681c86b16
Matthieu Oger <moger@pixelnest.io>
2013-07-26 16:13:30 +0200

    ScoreView
    * Use a XIB
    * Remove creation code
    * Refactoring

--- game/SuperKoikoukesse.iOS/Controllers/ScoreViewController.cs
+++ game/SuperKoikoukesse.iOS/Controllers/ScoreViewController.cs
@@ -12,8 +12,8 @@ namespace SuperKoikoukesse.iOS
   {
     #region Members
 
-    private Quizz mQuizz;
-    private static Dictionary<string, UIImage> mPlayerImageCache;
+    private Quizz _quizz;
+    private static Dictionary<string, UIImage> _playerImageCache;
 
     #endregion
 
@@ -21,16 +21,14 @@ namespace SuperKoikoukesse.iOS
 
     public ScoreViewController(Quizz q) : base ("ScoreView" + (AppDelegate.UserInterfaceIdiomIsPhone ? "_iPhone" : "_iPad"), null)
     {
-      mQuizz = q;
-      mPlayerImageCache = new Dictionary<string, UIImage>();
+      _quizz = q;
+      _playerImageCache = new Dictionary<string, UIImage>();
     }
 
     public override void ViewDidLoad()
     {
       base.ViewDidLoad();
-
-      //CreateView();
-      //SetViewDataFromQuizz();
+      //SetViewWithQuizz();
     }
 
     #endregion
@@ -42,112 +40,6 @@ namespace SuperKoikoukesse.iOS
       return AppDelegate.HasSupportedInterfaceOrientations();
     }
 
-    private void CreateView()
-    {
-      View = ViewTools.CreateUIView();
-
-      // Common
-      // ------------------------------------------
-
-      // Game over label
-      UILabel titleLabel = ViewTools.Createlabel(new RectangleF(10, 10, 150, 50), Localization.Get("game.over"));
-      View.AddSubview(titleLabel);
-
-      // Mode and difficulty
-      UILabel modeLabel = ViewTools.Createlabel(new RectangleF(10, 60, 150, 50), Localization.Get(mQuizz.Mode.ToString().ToLower() + ".title"));
-      View.AddSubview(modeLabel);
-      UILabel difficultyLabel = ViewTools.Createlabel(new RectangleF(10, 110, 150, 50), Localization.Get(mQuizz.Difficulty.ToString().ToLower() + ".title"));
-      View.AddSubview(difficultyLabel);
-
-      // Quit button
-      UIButton quitButton = ViewTools.CreateButton(
-        new RectangleF(10, 500, 200, 80), 
-        Localization.Get("button.quit"),
-        OnMenuPressed
-      );
-
-      View.AddSubview(quitButton);
-
-      if (mQuizz.Mode == GameModes.Versus)
-      {
-
-        // Game center versus
-        // ------------------------------------------
-        VersusMatch match = ProfileService.Instance.AuthenticatedPlayer.CurrentMatch;
-
-        // Game center match status
-        if (match.IsEnded)
-        {
-          // -- Match is over
-        }
-        else if (match.IsPlayerTurn(ProfileService.Instance.AuthenticatedPlayer.PlayerId) == false)
-        {
-          // -- Player is waiting for the opponent to play
-        }
-        else
-        {
-          // We should never be here but check...
-          Logger.Log(LogLevel.Error, "Match not ended and player turn: wtf are you doing in score view?");
-        }
-
-        // Load players avatar
-        if (mPlayerImageCache.ContainsKey(match.Player1Id) == false)
-        {
-          GKPlayer.LoadPlayersForIdentifiers(
-            new string[] { match.Player1Id },
-            LoadPlayers
-          );
-        }
-        if (mPlayerImageCache.ContainsKey(match.Player2Id) == false)
-        {
-          GKPlayer.LoadPlayersForIdentifiers(
-            new string[] { match.Player2Id },
-            LoadPlayers
-          );
-        }
-
-        // Game infos
-        // There is only two turns but here it's a generic data structure
-        foreach (var turn in match.Turns)
-        {
-          string playerId = turn.PlayerId;
-          int score = turn.Score;
-
-          UIImage avatar = null;
-          if (mPlayerImageCache.TryGetValue(playerId, out avatar) == false)
-          {
-            // Default avatar
-            avatar = new UIImage();
-          }
-        }
-      }
-      else
-      {
-        // Solo
-        // ------------------------------------------ 
-
-        // Score
-        // -- Title
-        UILabel localScoreTitle = ViewTools.Createlabel(new RectangleF(400, 50, 150, 50), Localization.Get("scores.local"));
-        View.AddSubview(localScoreTitle);
-
-        // -- Current score
-        UILabel currentScore = ViewTools.Createlabel(new RectangleF(400, 50, 150, 50), Localization.Get("scores.local"));
-        View.AddSubview(localScoreTitle);
-
-        // Game center scores
-
-        // Retry
-        UIButton retryButton = ViewTools.CreateImportantButton(
-          new RectangleF(10, 700, 200, 80), 
-          Localization.Get("button.retry"),
-          OnRetryPressed
-        );
-
-        View.AddSubview(retryButton);
-      }
-    }
-
     private void LoadPlayers(GKPlayer[] players, NSError error)
     {
 
@@ -155,7 +47,7 @@ namespace SuperKoikoukesse.iOS
       {
         player.LoadPhoto(GKPhotoSize.Normal, (image, loadImageError) => {
 
-          mPlayerImageCache.Add(player.PlayerID, image);
+          _playerImageCache.Add(player.PlayerID, image);
 
           BeginInvokeOnMainThread(() => {
             // TODO Refresh images
@@ -164,28 +56,27 @@ namespace SuperKoikoukesse.iOS
       }
     }
 
-    private void SetViewDataFromQuizz()
+    private void SetViewWithQuizz()
     {
       if (IsViewLoaded)
       {
 //        this.coinsLabel.Text = quizz.EarnedCoins.ToString ();
 
-        // Highscore control
+//        // Highscore control
 //        highScoreControl.SetScoreParameters (quizz.Mode, quizz.Difficulty, quizz.RankForLastScore, quizz.Score);
       }
-
     }
 
     #endregion
 
     #region Handlers
 
-    public void OnRetryPressed()
+    private void OnRetryTouched()
     {
-      if (ProfileService.Instance.CachedPlayer.Credits > 0)
+      if (PlayerCache.Instance.CachedPlayer.Credits > 0)
       {
         var appDelegate = (AppDelegate) UIApplication.SharedApplication.Delegate; 
-        appDelegate.SwitchToGameView(mQuizz.Mode, mQuizz.Difficulty, mQuizz.Filter);
+        appDelegate.SwitchToGameView(_quizz.Mode, _quizz.Difficulty, _quizz.Filter);
       }
       else
       {
@@ -193,7 +84,7 @@ namespace SuperKoikoukesse.iOS
       }
     }
 
-    public void OnMenuPressed()
+    private void OnMenuTouched()
     {
       var appDelegate = (AppDelegate) UIApplication.SharedApplication.Delegate; 
       appDelegate.SwitchToMenuView();
