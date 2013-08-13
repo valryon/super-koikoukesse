2a0d06e86b50e9e279e19959dfb6809dd32eeb3d
Matthieu Oger <moger@pixelnest.io>
2013-08-13 14:55:20 +0200

    Appearance

--- game/SuperKoikoukesse.iOS/Views/PXNAppearance.cs
+++ game/SuperKoikoukesse.iOS/Views/PXNAppearance.cs
@@ -15,13 +15,15 @@ namespace SuperKoikoukesse.iOS
     public PXNAppearance()
     {
       SetNavigationBarAppearance();
+      SetBarButtonItemAppearance();
+      SetBarBackButtonItemAppearance();
     }
 
     #endregion
 
     #region Methods
 
-    public void SetNavigationBarAppearance()
+    private void SetNavigationBarAppearance()
     {
       var attributes = new UITextAttributes();
       attributes.TextColor = UIColor.FromWhiteAlpha(white: 0.306f, alpha: 1f);
@@ -33,6 +35,30 @@ namespace SuperKoikoukesse.iOS
       UINavigationBar.Appearance.BackgroundColor = PXNConstants.COLOR_NAVIGATION;
       UINavigationBar.Appearance.SetTitleTextAttributes(attributes);
       UINavigationBar.Appearance.SetTitleVerticalPositionAdjustment(2f, UIBarMetrics.Default);
+    }
+
+    private void SetBarButtonItemAppearance()
+    {
+      var normalAttributes = new UITextAttributes();
+      normalAttributes.TextColor = PXNConstants.BRAND_COLOR;
+      normalAttributes.TextShadowOffset = new UIOffset(0, 0);
+      normalAttributes.TextShadowColor = UIColor.Clear;
+      normalAttributes.Font = UIFont.SystemFontOfSize(16);
+
+      var highlightAttributes = new UITextAttributes();
+      highlightAttributes.TextColor = UIColor.White;
+      highlightAttributes.TextShadowOffset = new UIOffset(0, 0);
+      highlightAttributes.TextShadowColor = UIColor.Clear;
+      highlightAttributes.Font = UIFont.SystemFontOfSize(16);
+
+      UIBarButtonItem.Appearance.SetTitleTextAttributes(normalAttributes, UIControlState.Normal);
+      UIBarButtonItem.Appearance.SetTitleTextAttributes(highlightAttributes, UIControlState.Highlighted);
+      UIBarButtonItem.Appearance.SetBackgroundImage(new UIImage(), UIControlState.Normal, UIBarMetrics.Default);
+    }
+
+    private void SetBarBackButtonItemAppearance()
+    {
+//      UIBarButtonItem.Appearance.SetBackButtonBackgroundImage(new UIImage(), UIControlState.Normal, UIBarMetrics.Default);
     }
 
     #endregion
