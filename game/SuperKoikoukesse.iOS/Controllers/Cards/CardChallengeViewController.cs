10b6385911215847f8d3c12efb6701ccb3c8bbc8
Matthieu Oger <moger@pixelnest.io>
2013-08-17 20:07:29 +0200

    Challenge view
    * New view
    * New controller
    * New resources
    * But I will redo everything with a collection view instead. So… it's
    almost useless… GG MATTHIEU.

---
+++ game/SuperKoikoukesse.iOS/Controllers/Cards/CardChallengeViewController.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Drawing;
+using MonoTouch.Foundation;
+using MonoTouch.UIKit;
+
+namespace SuperKoikoukesse.iOS
+{
+  public partial class CardChallengeViewController : AbstractCardViewController
+  {
+    #region Members
+    #endregion
+
+    #region Constructors
+
+    public CardChallengeViewController() : base("CardDifficultiesView")
+    {
+
+    }
+
+    #endregion
+
+    #region Methods
+    #endregion
+
+    #region Properties
+    #endregion
+  }
+}
+
