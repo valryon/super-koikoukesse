ac65d11861513b3fb795a799e9e9ff54b64575e7
Matthieu Oger <moger@pixelnest.io>
2013-08-27 20:58:30 +0200

    DifficultyView added

---
+++ game/SuperKoikoukesse.iOS/Views/Controls/DifficultyView.cs
@@ -0,0 +1,47 @@
+// Copyright (c) 2013 Copyright © 2013 Pixelnest Studio
+// This file is subject to the terms and conditions defined in
+// file 'LICENSE.md', which is part of this source code package.
+using System;
+using MonoTouch.UIKit;
+using MonoTouch.Foundation;
+
+namespace SuperKoikoukesse.iOS
+{
+  [Register("DifficultyView")]
+  public class DifficultyView : UIView
+  {
+    #region Members
+    #endregion
+
+    #region Constructors
+
+    public DifficultyView(IntPtr handle) : base(handle)
+    {
+
+    }
+
+    #endregion
+
+    #region Methods
+
+    public override void TouchesBegan(MonoTouch.Foundation.NSSet touches, UIEvent evt)
+    {
+      base.TouchesBegan(touches, evt);
+
+      BackgroundColor = PXNConstants.BRAND_COLOR;
+    }
+
+    public override void TouchesEnded(MonoTouch.Foundation.NSSet touches, UIEvent evt)
+    {
+      base.TouchesEnded(touches, evt);
+
+      BackgroundColor = UIColor.Clear;
+    }
+
+    #endregion
+
+    #region Properties
+    #endregion
+  }
+}
+
